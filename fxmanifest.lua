93b27b18bbddd65a2813f5d946365ca04e3c71f2
Case <90377400+CaseIRL@users.noreply.github.com>
2025-06-30 12:45:05 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,26 +1,35 @@
+--[[ 
+    This file is part of BDTK (BOII Development Tool Kit) and is licensed under the MIT License.
+    See the LICENSE file in the root directory for full terms.
+
+    © 2025 Case @ BOII Development
+
+    Support honest development — retain this credit. Don"t be that guy...
+]]
+
 --[[
----------------------------
-  ____   ____ _____ _____ 
- |  _ \ / __ \_   _|_   _|
- | |_) | |  | || |   | |  
- |  _ <| |  | || |   | |  
- | |_) | |__| || |_ _| |_ 
- |____/ \____/_____|_____|
-                                                    
----------------------------                                              
-      Utility Library
-          V2.1.0              
----------------------------
+#############################
+#  ____   ____ _____ _____  #
+# |  _ \ / __ \_   _|_   _| #
+# | |_) | |  | || |   | |   #
+# |  _ <| |  | || |   | |   #
+# | |_) | |__| || |_ _| |_  #
+# |____/ \____/_____|_____| #
+#                           #                       
+#############################                                         
+#           BDTK            #
+#          V0.1.2           #    
+#############################
 ]]
 
 fx_version "cerulean"
 games { "gta5", "rdr3" }
 
-name "boii_utils"
-version "2.2.0"
-description "Developer Utility Library."
+name "bdtk"
+version "0.1.2"
+description "A developer tool kit covering bridges and common functions."
 author "boiidevelopment"
-repository "https://github.com/boiidevelopment/boii_utils"
+repository "https://github.com/boiidevelopment/bdtk"
 lua54 "yes"
 
 fx_version "cerulean"
